8bca1007f931ad6c5749ff35af3cb67bb218c010
mladenm <rashtimovani@gmail.com>
2021-01-16 16:16:43 +0100

    Simulator and scaler.

---
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Scaler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace RasHack.GapOverlap.Main
+{
+    public class Scaler
+    {
+        private const float TARGET_WIDTH = 1920;
+        private const float TARGET_HEIGHT = 1080;
+
+        private readonly Camera mainCamera;
+        private readonly float depth;
+
+        public Scaler(Camera mainCamera, float depth)
+        {
+            this.mainCamera = mainCamera;
+            this.depth = depth;
+        }
+
+        public Vector3 BottomLeft => inDepth(mainCamera.ScreenToWorldPoint(
+            new Vector3(0, 0)));
+
+        public Vector3 BottomRight => inDepth(mainCamera.ScreenToWorldPoint(
+            new Vector3(Screen.width, 0)));
+
+        public Vector3 TopLeft => inDepth(mainCamera.ScreenToWorldPoint(
+            new Vector3(0, Screen.height)));
+
+        public Vector3 TopRight => inDepth(mainCamera.ScreenToWorldPoint(
+            new Vector3(Screen.width, Screen.height)));
+
+
+        private Vector3 inDepth(Vector3 original)
+        {
+            return new Vector3(original.x, original.y, depth);
+        }
+
+        public Vector3 point(Vector3 inputPosition)
+        {
+            return inDepth(mainCamera.ScreenToWorldPoint(inputPosition));
+        }
+    }
+}
---
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Simulator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RasHack.GapOverlap.Main
+{
+    public class Simulator : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer pointer;
+        [SerializeField] private SpriteRenderer bottomLeft;
+        [SerializeField] private SpriteRenderer bottomRight;
+        [SerializeField] private SpriteRenderer topLeft;
+        [SerializeField] private SpriteRenderer topRight;
+
+        [SerializeField] private bool showPointer;
+
+        private Scaler scaler;
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+            scaler = new Scaler(mainCamera, -1);
+
+            topLeft.transform.position = transform.InverseTransformPoint(scaler.TopLeft);
+            bottomLeft.transform.position = transform.InverseTransformPoint(scaler.BottomLeft);
+            bottomRight.transform.position = transform.InverseTransformPoint(scaler.BottomRight);
+            topRight.transform.position = transform.InverseTransformPoint(scaler.TopRight);
+        }
+
+        private void Update()
+        {
+            UpdateShownState();
+
+            pointer.transform.position = scaler.point(Input.mousePosition);
+        }
+
+        private void UpdateShownState()
+        {
+            pointer.enabled = showPointer;
+
+            bottomLeft.enabled = showPointer;
+            bottomRight.enabled = showPointer;
+            topLeft.enabled = showPointer;
+            topRight.enabled = showPointer;
+        }
+    }
+}
