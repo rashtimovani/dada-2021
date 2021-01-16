9f2e2d498d6192a1e73a3e2cc8221d799af2f5f7
mladenm <rashtimovani@gmail.com>
2021-01-16 14:37:01 +0100

    Added background for task.

---
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Background.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RasHack.GapOverlap.Main
+{
+    public enum BackgroundColor
+    {
+        Black = 0,
+        White = 1,
+    }
+
+    public class Background : MonoBehaviour
+    {
+        [SerializeField] private BackgroundColor usedColor = BackgroundColor.White;
+
+        [SerializeField] private Color black = Color.black;
+
+        [SerializeField] private Color white = Color.white;
+
+        [SerializeField] private SpriteRenderer sprite;
+
+        void Start()
+        {
+            switch (usedColor)
+            {
+                case BackgroundColor.Black:
+                    sprite.color = black;
+                    break;
+                default:
+                    sprite.color = white;
+                    break;
+            }
+        }
+    }
+}
