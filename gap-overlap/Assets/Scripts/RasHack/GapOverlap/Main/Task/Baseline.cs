ce85ad4d96fe11af9e328ba9f9e4d87a33b27b59
mladenm <rashtimovani@gmail.com>
2022-05-22 14:32:27 +0200

    Implement basic functionality of baseline test.

--- gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Task/Baseline.cs
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Task/Baseline.cs
@@ -21,6 +21,16 @@ namespace RasHack.GapOverlap.Main.Task
 
         #endregion
 
+        #region Fields
+
+        private float? centralTimeOnly;
+        private float? outInTime;
+        private float? measurement;
+        private CentralStimulus centralStimulus;
+        private Stimulus activeStimulus;
+
+        #endregion
+
         #region API
 
         protected override TaskType TaskType => TaskType.Baseline;
@@ -29,24 +39,84 @@ namespace RasHack.GapOverlap.Main.Task
 
         public override void ReportFocusedOn(Stimulus stimulus, float after)
         {
-            throw new NotImplementedException();
+            if (stimulus != activeStimulus)
+            {
+                Debug.LogError($"{stimulus} stimulus is not the active one, don't care if it reported focused!");
+                return;
+            }
+
+            measurement = after;
+            Debug.Log($"{stimulus} reported focused after {after:0.000}s!");
+        }
+        
+        public override void ReportStimulusDied(Stimulus active)
+        {
+            if (active != activeStimulus)
+            {
+                Debug.LogError($"{active} stimulus is not the active one, don't care if it died!");
+                return;
+            }
+
+            Debug.Log($"{activeStimulus} has finished");
+            Destroy(activeStimulus.gameObject);
+            activeStimulus = null;
+            owner.ReportTaskFinished(this, measurement);
+            Destroy(gameObject);
         }
 
         public override void ReportCentralStimulusDied(CentralStimulus central)
         {
-            throw new NotImplementedException();
+            if (central != centralStimulus)
+            {
+                Debug.LogError($"{central} stimulus is not the central one, don't care if it died!");
+                return;
+            }
+
+            Debug.Log($"{centralStimulus} has finished");
+            Destroy(centralStimulus.gameObject);
         }
 
-        public override void ReportStimulusDied(Stimulus active)
+        #endregion
+        
+        #region Mono methods
+        
+        private void Start()
         {
-            throw new NotImplementedException();
+            StartWithCentralStimulus();
+        }
+
+        private void Update()
+        {
+            if (!centralTimeOnly.HasValue) return;
+            centralTimeOnly -= Time.deltaTime;
+            if (centralTimeOnly > 0f) return;
+            centralTimeOnly = null;
+            StartWithStimulus();
         }
 
         protected override void OnDestroy()
         {
-            throw new NotImplementedException();
+            if (centralStimulus != null) Destroy(centralStimulus.gameObject);
+            if (activeStimulus != null) Destroy(activeStimulus.gameObject);
         }
         
+        #endregion
+        
+        #region Helpers
+
+        private void StartWithCentralStimulus()
+        {
+            centralStimulus = NewCentralStimulus();
+            centralStimulus.StartSimulating(this, Times.CentralTime);
+            centralTimeOnly = Times.CentralTime;
+        }
+
+        private void StartWithStimulus()
+        {
+            activeStimulus = NewStimulus();
+            activeStimulus.StartSimulating(stimulusType, this, Times.StimulusTime);
+        }
+
         #endregion
     }
 }
