483f1d602e037b587b0797fa4dd0c968c83b6f68
mladenm <rashtimovani@gmail.com>
2022-06-23 19:53:44 +0200

    Fix inheritance for touch devices.

--- gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Inputs/TouchPointer.cs
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/Inputs/TouchPointer.cs
@@ -3,7 +3,7 @@ using UnityEngine.XR;
 
 namespace RasHack.GapOverlap.Main.Inputs
 {
-    public class TouchPointer : InputDevices
+    public class TouchPointer : PointerDevice
     {
         public Vector3? Position =>
             Input.touchSupported && Input.touchCount > 0 ? Input.touches[0].position : (Vector3?) null;
