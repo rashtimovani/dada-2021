63d76eb5f1c9071309bb6e57f34c29f41460c3af
rashtimovani <rashtimovani@gmail.com>
2022-12-24 11:18:19 +0100

    [#11] Control flow of calibration done.

--- gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/CalibrationFinisher.cs
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/CalibrationFinisher.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Tobii.Research;
 using Tobii.Research.Unity;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
 
 namespace RasHack.GapOverlap.Main
 {
@@ -12,6 +8,7 @@ namespace RasHack.GapOverlap.Main
     {
         Initialized,
         Running,
+        Message,
         Done
     }
 
@@ -22,27 +19,66 @@ namespace RasHack.GapOverlap.Main
         [SerializeField]
         private Calibration calibration;
 
+        [SerializeField]
+        private EyeTracker tracker;
+
+        [SerializeField]
+        private float messageDuration = 5f;
+
         private CalibrationState state = CalibrationState.Initialized;
+        private float messageRemaining;
 
         #endregion
 
         #region Mono methods
+
+        private void Start()
+        {
+            HideMessage();
+        }
+
         private void Update()
         {
             switch (state)
             {
                 case CalibrationState.Initialized:
-                    if (calibration.StartCalibration()) state = CalibrationState.Running;
+                    if (tracker.EyeTrackerInterface == null) DisplayMessage("No eye tracker detected!");
+                    else if (calibration.StartCalibration()) state = CalibrationState.Running;
+                    else DisplayMessage("Calibration is already running");
                     break;
                 case CalibrationState.Running:
-                    if (!calibration.CalibrationInProgress) state = CalibrationState.Done;
+                    if (!calibration.CalibrationInProgress) DisplayMessage(calibration.LatestCalibrationSuccessful ? "Calibration was succcessful" : "Calibration failed!");
+                    break;
+                case CalibrationState.Message:
+                    messageRemaining -= Time.deltaTime;
+                    if (messageRemaining <= 0)
+                    {
+                        state = CalibrationState.Done;
+                        HideMessage();
+                    }
                     break;
                 default:
+                    SceneManager.LoadScene(0); // Main scene
                     break;
-
             }
         }
 
         #endregion 
+
+        #region Helper methods
+
+        private void HideMessage()
+        {
+            messageRemaining = 0f;
+        }
+
+        private void DisplayMessage(string message)
+        {
+            state = CalibrationState.Message;
+            messageRemaining = messageDuration;
+            Debug.Log(message);
+        }
+
+        #endregion
     }
 }
