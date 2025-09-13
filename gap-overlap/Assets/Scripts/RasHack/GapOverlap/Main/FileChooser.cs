c98ee3fe715c2e9685a3784eafe923d832042ffc
rashtimovani <rashtimovani@gmail.com>
2025-09-13 09:42:20 +0200

    Put all jsons in chooser dropdown.

--- gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/FileChooser.cs
+++ gap-overlap/Assets/Scripts/RasHack/GapOverlap/Main/FileChooser.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RasHack.GapOverlap.Main
 {
@@ -8,18 +10,61 @@ namespace RasHack.GapOverlap.Main
 
         [SerializeField] private GameObject panel;
 
+        [SerializeField] private TMP_Dropdown dropdown;
+
+        [SerializeField] private InputField folderInput;
+
+        private HomeUI home;
+
+        #endregion
+
+        #region Fields
+
+        private int selectedItem;
+
         #endregion
 
         #region Methods
 
+        private void Start()
+        {
+            home = FindObjectOfType<HomeUI>();
+        }
+
+        public void OnBack()
+        {
+            Hide();
+            home.Show();
+        }
+
         public void Show()
         {
+            LoadFiles();
             panel.SetActive(true);
         }
 
         public void Hide()
         {
             panel.SetActive(false);
+        }
+
+        public void LoadFiles()
+        {
+            string folderPath = folderInput.text;
+            string[] jsonFiles = System.IO.Directory.GetFiles(folderPath, "*.json", System.IO.SearchOption.AllDirectories);
+
+            selectedItem = dropdown.value;
+
+            dropdown.ClearOptions();
+            // Example: print file paths to console
+            foreach (string file in jsonFiles)
+            {
+                dropdown.options.Add(new TMP_Dropdown.OptionData() { text = file });
+            }
+            if (dropdown.options.Count > selectedItem)
+            {
+                dropdown.value = selectedItem;
+            }
         }
 
         #endregion
